c971c42cb32d03eae86e10f52812e0a0b30d949f
Jonanabanana <joannarubivic@gmail.com>
2016-09-30 17:06:50 +1300

    animations added, death in place, other cool things on trello done

---
+++ Assets/Scripts/GamePlayCntrl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GamePlayCntrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnEnable(){
+		BoatObj.playerDied += PlayerDied;
+	}
+
+	void OnDisable(){
+		BoatObj.playerDied -= PlayerDied;
+	}
+
+	void PlayerDied(){
+		StartCoroutine (RestartGame ());
+	}
+	IEnumerator RestartGame(){
+		yield return new WaitForSeconds (2f);
+		SceneManager.LoadScene ("FishGame");
+	}
+	// Update is called once per frame
+	void Update () {
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
---
+++ Assets/Scripts/levelSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class levelSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			SceneManager.LoadScene ("level1");
+		}
+	}
+	public void loadScene (string sceneName){
+		SceneManager.LoadScene (sceneName);
+	}
+}
